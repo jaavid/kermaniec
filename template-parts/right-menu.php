d5ab9d2a318baf54760a9da19fc94e62e98b80fc
jaavid <javid.momeni@gmail.com>
2019-02-24 09:40:55 +0330

    static right menu

---
+++ template-parts/right-menu.php
@@ -0,0 +1,59 @@
+<?php 
+	#TODO: Make Dynamic Menu
+	#TODO: Write Wordpress Menu Walker Class
+?>
+<div class="ui vertical fluid inverted purple menu">
+  <div class="item">
+    <div class="ui icon input">
+    	<input type="text" placeholder="جستجو...">
+    	<i class="search icon"></i>
+    </div>
+  </div>				  
+  <a href="https://iec.kr.ir/ceo-message" class="item">
+    <i class="envelope open text icon"></i> پیام مدیرعامل
+  </a>
+  <a href="https://iec.kr.ir/download/1.zip" class="item">
+    <i class="hand holding usd icon"></i> الویت های سرمایه گذاری
+  </a>				  
+  <a href="https://iec.kr.ir/archive/industrial-area" class="item">
+    <i class="city icon"></i> شهرک ها و نواحی صنعتی
+  </a>				  				  				  
+  <a href="https://iec.kr.ir/eforms" class="item">
+    <i class="grid layout icon"></i> فرم خدمات الکترونیک
+  </a>
+  <a href="https://iec.kr.ir/applicants-guide/iec-64-kerman.html" class="item">
+    <i class="grid layout icon"></i> رسیدگی به شکایات
+  </a>
+  <a href="http://new.isipo.ir/index.jsp?siteid=1&fkeyid=&siteid=1&pageid=416" class="item">
+    <i class="grid layout icon"></i> خدمات الکترونیک
+  </a>   
+  <a href="https://iec.kr.ir/%d8%a7%d8%b1%d8%aa%d8%a8%d8%a7%d8%b7-%d8%a8%d8%a7-%d9%86%d8%ae%d8%a8%da%af%d8%a7%d9%86-%d9%88-%d8%b5%d8%a7%d8%ad%d8%a8-%d9%86%d8%b8%d8%b1%d8%a7%d9%86-%d8%ad%d9%88%d8%b2%d9%87-%d8%b5%d9%86%d8%b9%d8%aa" class="item">
+    <i class="envelope icon"></i> ارتباط با نخبگان
+  </a>
+  <a href="https://iec.kr.ir/faq/iec-36-kerman.html" class="item">
+    <i class="info icon"></i> سوالات متداول
+  </a>
+  <a href="https://iec.kr.ir/albums" class="item">
+    <i class="images icon"></i> گالری تصاویر
+  </a>
+  <a href="https://iec.kr.ir/%d8%a7%d9%85%d9%88%d8%b1-%d9%81%d8%b1%d9%87%d9%86%da%af%db%8c" class="item">
+    <i class="grid layout icon"></i> امور فرهنگی
+  </a>
+  <a href="https://iec.kr.ir/2014-12-17-16-00-29" class="item">
+    <i class="grid layout icon"></i> بسیج
+  </a>
+  <div class="item">
+    معاونت ها
+    <div class="menu">
+      <a href="https://iec.kr.ir/planning" class="item">
+	    <i class="grid layout icon"></i> معاونت برنامه ریزی و امور اقتصادی
+	  </a>
+	  <a href="https://iec.kr.ir/archive/small-industries" class="item">
+	    <i class="grid layout icon"></i> معاونت صنایع کوچک
+	  </a>
+	  <a href="https://iec.kr.ir/%d9%85%d8%b9%d8%a7%d9%88%d9%86%d8%aa-%d8%b9%d9%85%d8%b1%d8%a7%d9%86-%d9%88-%d9%85%d8%ad%db%8c%d8%b7-%d8%b2%db%8c%d8%b3%d8%aa" class="item">
+	    <i class="grid layout icon"></i> معاونت فنی
+	  </a>		
+    </div>
+  </div>
+</div>
